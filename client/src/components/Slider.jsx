90d8e96d77f0db1023337f4ad385b660770fb496
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-08 13:31:57 +0300

    Created the Slider

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,56 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React from "react";
+import styles from "./Slider.module.css";
+import sliderImage from "../assets/slider-image.png";
+import { useState } from "react";
+import { sliderItems } from "../sliderData";
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const sliderHandler = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div
+        className={`${styles.arrow} ${styles["arrow-left"]}`}
+        onClick={() => sliderHandler("left")}
+      >
+        <ArrowLeftOutlined />
+      </div>
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${slideIndex * -100}vw)` }}
+      >
+        {sliderItems.map((slide) => (
+          <div
+            className={styles.slide}
+            style={{ backgroundColor: `#${slide.bg}` }}
+          >
+            <div className={styles["image-container"]}>
+              <img className={styles.image} src={sliderImage} alt="" />
+            </div>
+            <div className={styles["slider-container"]}>
+              <h1 className={styles["slider-header"]}>{slide.title}</h1>
+              <p className={styles["slider-description"]}>{slide.desc}</p>
+              <button className={styles["slider-button"]}>Lorem</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div
+        className={`${styles.arrow} ${styles["arrow-right"]}`}
+        onClick={() => sliderHandler("right")}
+      >
+        <ArrowRightOutlined />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
