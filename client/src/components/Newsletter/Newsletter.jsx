cc8bd35df997a98f6f620ee25b84e2457491b456
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-03-08 15:46:47 +0300

    Created Newsletter

---
+++ client/src/components/Newsletter/Newsletter.jsx
@@ -0,0 +1,22 @@
+import { Send } from "@material-ui/icons";
+import React from "react";
+import styles from "./Newsletter.module.css";
+
+const Newsletter = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Newsletter</h1>
+      <p className={styles.description}>
+        Get timely updates from your favorite products.
+      </p>
+      <div className={styles["input-container"]}>
+        <input type="text" className={styles.input} placeholder="Your Email" />
+        <button className={styles.button}>
+          <Send />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Newsletter;
